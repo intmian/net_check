5cac412a5449d2e59b51cb3ee06bbf8de04cc52a
intmian <1437131326@qq.com>
2018-11-13 16:03:50 +0800

    重构并添加了架构

--- net_check/net_check.py
+++ net_check/net_check.py
@@ -4,71 +4,71 @@ import threading
 from io import BytesIO
 import os
 import threading
-def clear():os.system('cls')
 import time
 import json
 import sys
 import shutil
 
-
+def clear():os.system('cls')
 threads_information = []
-dict_code = {100: "Continue",
-             101: "Switching Protocols",
-             102: "Processing",
-             200: "OK",
-             201: "Created",
-             202: "Accepted",
-             203: "Non-Authoritative Information",
-             204: "No Content",
-             205: "Reset Content",
-             206: "Partial Content",
-             207: "Multi-Status",
-             300: "Multiple Choices",
-             301: "Moved Permanently",
-             302: "Move temporarily",
-             303: "See Other",
-             304: "Not Modified",
-             305: "Use Proxy",
-             306: "Switch Proxy",
-             307: "Temporary Redirect",
-             400: "Bad Request",
-             401: "Unauthorized",
-             402: "Payment Required",
-             403: "Forbidden",
-             404: "Not Found",
-             405: "Method Not Allowed",
-             406: "Not Acceptable",
-             407: "Proxy Authentication Required",
-             408: "Request Timeout",
-             409: "Conflict",
-             410: "Gone",
-             411: "Length Required",
-             412: "Precondition Failed",
-             413: "Request Entity Too Large",
-             414: "Request-URI Too Long",
-             415: "Unsupported Media Type",
-             416: "Requested Range Not Satisfiable",
-             417: "Expectation Failed",
-             421: "too many connections",
-             422: "Unprocessable Entity",
-             423: "Locked",
-             424: "Failed Dependency",
-             425: "Unordered Collection",
-             426: "Upgrade Required",
-             449: "Retry With",
-             451: "Unavailable For Legal Reasons",
-             500: "Internal Server Error",
-             501: "Not Implemented",
-             502: "Bad Gateway",
-             503: "Service Unavailable",
-             504: "Gateway Timeout",
-             505: "HTTP Version Not Supported",
-             506: "Variant Also Negotiates",
-             507: "Insufficient Storage",
-             509: "Bandwidth Limit Exceeded",
-             510: "Not Extended",
-             600: "Unparseable Response Headers"
-             }
+dict_code = {
+    100: "Continue",
+    101: "Switching Protocols",
+    102: "Processing",
+    200: "OK",
+    201: "Created",
+    202: "Accepted",
+    203: "Non-Authoritative Information",
+    204: "No Content",
+    205: "Reset Content",
+    206: "Partial Content",
+    207: "Multi-Status",
+    300: "Multiple Choices",
+    301: "Moved Permanently",
+    302: "Move temporarily",
+    303: "See Other",
+    304: "Not Modified",
+    305: "Use Proxy",
+    306: "Switch Proxy",
+    307: "Temporary Redirect",
+    400: "Bad Request",
+    401: "Unauthorized",
+    402: "Payment Required",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    406: "Not Acceptable",
+    407: "Proxy Authentication Required",
+    408: "Request Timeout",
+    409: "Conflict",
+    410: "Gone",
+    411: "Length Required",
+    412: "Precondition Failed",
+    413: "Request Entity Too Large",
+    414: "Request-URI Too Long",
+    415: "Unsupported Media Type",
+    416: "Requested Range Not Satisfiable",
+    417: "Expectation Failed",
+    421: "too many connections",
+    422: "Unprocessable Entity",
+    423: "Locked",
+    424: "Failed Dependency",
+    425: "Unordered Collection",
+    426: "Upgrade Required",
+    449: "Retry With",
+    451: "Unavailable For Legal Reasons",
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    505: "HTTP Version Not Supported",
+    506: "Variant Also Negotiates",
+    507: "Insufficient Storage",
+    509: "Bandwidth Limit Exceeded",
+    510: "Not Extended",
+    600: "Unparseable Response Headers"
+    }
 
 test_urls = None
 # TODO read it by read set
@@ -76,6 +76,7 @@ thread_num = None
 port = None
 work_add = None
 config_add = None
+
 test_urls_default = ["https://www.baidu.com", 
     "https://www.intmian.com", 
     "https://github.com", 
@@ -196,6 +197,22 @@ def GetSet():
     with open(config_add + "\\port.json","r",encoding='utf-8') as f:
         port = str(json.load(f))
         print("线程数已读入")
+
+def TestWebSetForTime(time_divide):  # 接受time_divide作为间隔时间，类型：int
+    
+    # TODO
+    pass
+
+def TestWebSetForTimeOut():  # 外部接口
+    second = input("请输入两次测试的间隔时间")
+    clear()
+    print("请输入ctrl + c来结束测试")
+    time.sleep(3)
+    clear()
+    print("测试开始")
+    TestWebSetForTime(int(second))
+    # TODO
+
 
 def PushSetToFile():
     with open(config_add + "\\websites.json","w") as f:
